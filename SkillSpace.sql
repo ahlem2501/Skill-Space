a33f9c5229f3e8450c284815dc6586039baa598e
fatmata319 <bahfatmata@iut-dhaka.edu>
2024-10-16 15:54:10 +0600

    Update SkillSpace.sql

--- SkillSpace.sql
+++ SkillSpace.sql
@@ -7,13 +7,13 @@ CREATE TABLE `lesson` (
   `course_name` text COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 CREATE TABLE `course` (
-  `course_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL AUTO_INCREMENT,  -- Added AUTO_INCREMENT for unique IDs
   `course_name` text COLLATE utf8_bin NOT NULL,
   `course_desc` text COLLATE utf8_bin NOT NULL,
   `course_author` varchar(255) COLLATE utf8_bin NOT NULL,
   `course_img` text COLLATE utf8_bin NOT NULL,
   `course_duration` text COLLATE utf8_bin NOT NULL,
   `course_price` int(11) NOT NULL,
-  `course_original_price` int(11) NOT NULL
+  `course_original_price` int(11) NOT NULL,
+  PRIMARY KEY (`course_id`)  -- Set course_id as the primary key
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
