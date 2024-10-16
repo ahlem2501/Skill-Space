14211fe981652cbc8dd5ea39c9ef983689ed7566
Doumbia02 <mansoureh225@gmail.com>
2024-10-16 15:39:43 +0600

    lesson
    new

---
+++ SkillSpace.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `lesson` (
+  `lesson_id` int(11) NOT NULL,
+  `lesson_name` text COLLATE utf8_bin NOT NULL,
+  `lesson_desc` text COLLATE utf8_bin NOT NULL,
+  `lesson_link` text COLLATE utf8_bin NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `course_name` text COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
