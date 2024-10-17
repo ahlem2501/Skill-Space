20bfd1b99c5e529b13793c9731ef5768cb2bbc06
Doumbia02 <mansoureh225@gmail.com>
2024-10-17 22:42:53 +0600

    Update index.php

--- index.php
+++ index.php
@@ -45,19 +45,9 @@
         <div class="row text-center">
           <div class="col-sm">
             <h5>About Us</h5>
-              <p>SkillSpace provides universal access to the world’s best education, partnering with top universities and organizations to offer courses online.</p>
-          </div>
-          <div class="col-sm">
-            <h5>Category</h5>
-            <a class="text-dark" href="#">Web Development</a><br />
-            <a class="text-dark" href="#">Web Designing</a><br />
-            <a class="text-dark" href="#">Android App Dev</a><br />
-            <a class="text-dark" href="#">iOS Development</a><br />
-            <a class="text-dark" href="#">Data Analysis</a><br />
           </div>
           <div class="col-sm">
             <h5>Contact Us</h5>
-            <p>SkillSpace Pvt Ltd <br> Near Police Camp II <br> Bokaro, Jharkhand <br> Ph. 000000000 </p>
           </div>
         </div>
       </div>
