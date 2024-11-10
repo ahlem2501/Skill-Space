61ecca925148f33f786232e6387b20fd4a0141b6
ZOUNGRANA ALIMATA <142305844+ZoungranaAlimata@users.noreply.github.com>
2024-11-10 21:11:52 +0600

    Merge branch 'main' of https://github.com/ahlem2501/Skill-Space

--- index.php
+++ index.php
@@ -89,6 +89,126 @@
 
 <!--end banner -->
 
+ <!-- Start Most  Popular Course -->
+
+ <div class="container mt-5">
+              <h1 class="text-center">Popular Course</h1>
+
+        <!-- Start Most  Popular Course  1st categorie Language Programming-->
+       <div class="card-deck mt-4">
+          <a href="#" class="btn" style="text-align: left; 
+          padding:0px; margin:0px">
+          <div class="Card">
+            <img src="image/course image/CSE.jpeg" class="card-img-top" alt="C language">
+             <div class="card-body">
+                <h5 class="card-title"> </h5> learn C language progrram
+                <p class="card-text">asdfdfghjkdfgjhkssdfghj</p>
+              </div>
+              <div class="card-footer">
+              <a class="btn btn-primary text-white font-weigth-bolder float-right" href="#">Enroll</a>
+
+              </div>
+          </div>
+            
+          </a>
+
+          <a href="#" class="btn" style="text-align: left; 
+          padding:0px; margin:0px">
+          <div class="Card">
+            <img src="image/course image/CSE.jpeg" class="card-img-top" alt="C language">
+             <div class="card-body">
+                <h5 class="card-title"> </h5> learn C language progrram
+                <p class="card-text">asdfdfghjkdfgjhkssdfghj</p>
+              </div>
+              <div class="card-footer">
+              <a class="btn btn-primary text-white font-weigth-bolder float-right" href="#">Enroll</a>
+
+              </div>
+          </div>
+            
+          </a>
+
+
+          <a href="#" class="btn" style="text-align: left; 
+          padding:0px; margin:0px">
+          <div class="Card">
+            <img src="image/course image/CSE.jpeg" class="card-img-top" alt="C language">
+             <div class="card-body">
+                <h5 class="card-title"> </h5> learn C language progrram
+                <p class="card-text">asdfdfghjkdfgjhkssdfghj</p>
+              </div>
+              <div class="card-footer">
+              <a class="btn btn-primary text-white font-weigth-bolder float-right" href="#">Enroll</a>
+
+              </div>
+          </div>
+            
+          </a>
+        </div>
+
+         <!-- end Most  Popular Course  1st categorie Language Programming-->
+
+                 <!-- Start Most  Popular Course  2st categorie Language Programming-->
+
+                 <div class="card-deck mt-4">
+          <a href="#" class="btn" style="text-align: left; 
+          padding:0px; margin:0px">
+          <div class="Card">
+            <img src="image/course image/images.jpeg" class="card-img-top" alt="JAVA Language">
+             <div class="card-body">
+                <h5 class="card-title"> </h5> learn JAVA language progrram
+                <p class="card-text">asdfdfghjkdfgjhkssdfghj</p>
+              </div>
+              <div class="card-footer">
+              <a class="btn btn-primary text-white font-weigth-bolder float-right" href="#">Enroll</a>
+
+              </div>
+          </div>
+            
+          </a>
+
+          <a href="#" class="btn" style="text-align: left; 
+          padding:0px; margin:0px">
+          <div class="Card">
+            <img src="image/course image/images.jpeg" class="card-img-top" alt="JAVA Language">
+             <div class="card-body">
+                <h5 class="card-title"> </h5> learn JAVA language progrram
+                <p class="card-text">asdfdfghjkdfgjhkssdfghj</p>
+              </div>
+              <div class="card-footer">
+              <a class="btn btn-primary text-white font-weigth-bolder float-right" href="#">Enroll</a>
+
+              </div>
+          </div>
+            
+          </a>
+
+          <a href="#" class="btn" style="text-align: left; 
+          padding:0px; margin:0px">
+          <div class="Card">
+            <img src="image/course image/images.jpeg" class="card-img-top" alt="JAVA Language">
+             <div class="card-body">
+                <h5 class="card-title"> </h5> learn JAVA language progrram
+                <p class="card-text">asdfdfghjkdfgjhkssdfghj</p>
+              </div>
+              <div class="card-footer">
+              <a class="btn btn-primary text-white font-weigth-bolder float-right" href="#">Enroll</a>
+
+              </div>
+          </div>
+            
+          </a>
+
+        </div>
+ <!-- end Most  Popular Course  2st categorie Language Programming-->
+  <div class="text-center m-2">
+    <a class="btn-danger btn-sm" href="#"> View all course</a>
+  </div>
+</div>
+
+  <!--end Most  Popular Course -->
+
+
 
 
 <div class="container-fluid bg-danger"> <!-- Start Social Follow -->
