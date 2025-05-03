9cdcef0d766a7468e0b1eb74e01ec022abdb9527
Doumbia02 <mansoureh225@gmail.com>
2025-05-03 22:44:24 +0600

    dbconnection U

--- dbConnection.php
+++ dbConnection.php
@@ -1,12 +1,13 @@
+<!-- C:\xampp\htdocs\Educode_update\Educode\ELearning\dbConnection.php -->
 <?php
 $db_host = "localhost";
 $db_user = "root";
 $db_password = "";
-$db_name = "skill_space";
+$db_name = "lms_db1";
 
 // Create Connection
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
-
+// $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 // Check Connection
 if($conn->connect_error) {
  die("connection failed");
